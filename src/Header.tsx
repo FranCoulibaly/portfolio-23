9b4c0e4fff80757a72cb8e99256aa328b8297123
Francesca Coulibaly <fran.coulibaly@gmail.com>
2023-02-24 16:21:20 +0100

    header v1

--- src/Header.tsx
+++ src/Header.tsx
@@ -12,66 +12,81 @@ const Header = () => {
     const container = useRef<HTMLDivElement>(null);
     const windowSize = useRef([window.innerWidth, window.innerHeight]);
     const nameRef = useRef<HTMLDivElement>(null);
+
+    const letterRefs: any[] = [];
+    // letterRefs.current = [];
     let q = gsap.utils.selector(nameRef);
 
     let name: string = "FRANCESCA COULIBALY";
     let nameArr: string[] = name.split('');
-    const letterRefs: any[] = [];
+   
 
     useLayoutEffect(function(): any {
         let width = windowSize.current[0];
         let height= windowSize.current[1];
-        let x = (): number => {
-            return gsap.utils.random(0, width - 200);
-            }
-        let y = (): number => {
-            return gsap.utils.random(0, height - 100);
-            }
+        let floating: any = null;
+        let tl = gsap.timeline();
 
         let ctx = gsap.context(() => {
-            
-            gsap.set(q(".letter"), {x: x, y: y});
+            letterRefs.forEach(letter => {
+                let x = (): number => {
+                    return gsap.utils.random(0, width - 200);
+                    }
+                let y = (): number => {
+                    return gsap.utils.random(0, height - 100);
+                    }
+            gsap.set(letter.current, {x: x, y: y});
 
-            let floating = gsap.to(q(".letter"), {
-                onComplete: () => {floating.reverse()},
+            const floatingFunc: any = () =>  {
+                floating = tl.to(letter.current, {
+                onComplete: floatingFunc,
                 ease: "none",
-                duration: 4,
-                x: x,
-                y: y,
+                duration: "random(5, 20)",
+                x: gsap.utils.random(0, width-400),
+                y: gsap.utils.random([0, height - 100]),
+                repeat: -1,
+                yoyo: true,
                 
-            }); 
-
-           let scrollAnim =  gsap.to(q(".letter"), {
-                x: (): number => {
-                    if (nameRef.current){
-                        // floating.pause();
-                        let nameWidth = nameRef.current.clientWidth;
-                        return (width/2) - (nameWidth/2);
-                     } else {
-                        return width/1.5
-                     } 
-                },
-                y: (): number  => {
-                    if (nameRef.current){
-                        let nameHeight = nameRef.current.clientHeight;
-                        return (height/2) - (nameHeight/2)
-                     } else {
-                        return height/1.5
-                     }
-                },
-                duration: 3,
-            });
-
-            ScrollTrigger.create ({
-                animation: scrollAnim,
-                onEnter: () => { floating.pause()},
-                trigger: ".container",
-                start: "+=1",
-                end: "+=400",
-                markers: true,
-                pin: ".container",
-            });          
+                
+            }, 0);
+            
+            }
+            return floatingFunc();          
         });
+        // tl.repeat(-1);
+        let scrollAnim =  gsap.to(q(".letter"), {
+            x: (): number => {
+                if (nameRef.current){
+                    let nameWidth = nameRef.current.clientWidth;
+                    return (width/2) - (nameWidth/2);
+                 } else {
+                    return width/1.5
+                 } 
+            },
+            y: (): number  => {
+                if (nameRef.current){
+                    let nameHeight = nameRef.current.clientHeight;
+                    return (height/2) - (nameHeight/2)
+                 } else {
+                    return height/1.5
+                 }
+            },
+            duration: 3,
+            
+        });
+        
+        
+        ScrollTrigger.create ({
+            animation: scrollAnim,
+            onEnter: () => {tl.pause()},
+            trigger: ".container",
+            start: "+=1",
+            end: "+=400",
+            markers: true,
+            pin: ".container",
+            // pinSpacing: false,
+        });
+    });
 
         return () => ctx.revert(); 
     }, []);
