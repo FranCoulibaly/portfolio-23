bd3f37b8b772096535082afa9850d08ed7b99bcc
Francesca Coulibaly <fran.coulibaly@gmail.com>
2023-02-28 15:57:57 +0100

    header animation v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { createRef, HTMLAttributes, useLayoutEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import Header from './Header';
+import About from './About';
 import './App.css';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import { gsap } from "gsap";
@@ -11,6 +12,7 @@ function App() {
     <div className="App" >
         <div >
           <Header />
+          <About />
         </div>
     </div>
   );
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useLayoutEffect, useRef, createRef, useState } from "react";
+import React, { ReactNode, useLayoutEffect, useRef, createRef, useState, MouseEventHandler } from "react";
 import { gsap } from "gsap";
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
@@ -9,32 +9,43 @@ export interface Props {
 }
 
 const Header = () => {
+    const [scrollState, setScrollState] = useState(false);
     const container = useRef<HTMLDivElement>(null);
     const windowSize = useRef([window.innerWidth, window.innerHeight]);
     const nameRef = useRef<HTMLDivElement>(null);
+    const titleRef = useRef<HTMLDivElement>(null);
 
-    const letterRefs: any[] = [];
+    const nameLetterRefs: any[] = [];
+    const titleLetterRefs: any[] = [];
     // letterRefs.current = [];
-    let q = gsap.utils.selector(nameRef);
+    let q = gsap.utils.selector(container);
 
     let name: string = "FRANCESCA COULIBALY";
+    let title: string = "FRONTEND DEVELOPER"
     let nameArr: string[] = name.split('');
+    let titleArr: string[] = title.split('');
+    let tl3 = gsap.timeline();
    
 
     useLayoutEffect(function(): any {
         let width = windowSize.current[0];
-        let height= windowSize.current[1];
+        let height = windowSize.current[1];
         let floating: any = null;
-        let tl = gsap.timeline();
+        
 
         let ctx = gsap.context(() => {
-            letterRefs.forEach(letter => {
-                let x = (): number => {
-                    return gsap.utils.random(0, width - 200);
-                    }
-                let y = (): number => {
-                    return gsap.utils.random(0, height - 100);
-                    }
+            
+
+            let x = (): number => {
+                return gsap.utils.random(0, width - 400);
+                }
+            let y = (): number => {
+                return gsap.utils.random(0, height - 100);
+                }
+            
+            let tl = gsap.timeline();
+            nameLetterRefs.forEach(letter => {
+                
             gsap.set(letter.current, {x: x, y: y});
 
             const floatingFunc: any = () =>  {
@@ -42,43 +53,82 @@ const Header = () => {
                 onComplete: floatingFunc,
                 ease: "none",
                 duration: "random(5, 20)",
-                x: gsap.utils.random(0, width-400),
-                y: gsap.utils.random([0, height - 100]),
+                x: x,
+                y: y,
                 repeat: -1,
                 yoyo: true,
-                
-                
             }, 0);
-            
             }
             return floatingFunc();          
         });
-        // tl.repeat(-1);
-        let scrollAnim =  gsap.to(q(".letter"), {
-            x: (): number => {
-                if (nameRef.current){
-                    let nameWidth = nameRef.current.clientWidth;
-                    return (width/2) - (nameWidth/2);
-                 } else {
-                    return width/1.5
-                 } 
-            },
-            y: (): number  => {
-                if (nameRef.current){
-                    let nameHeight = nameRef.current.clientHeight;
-                    return (height/2) - (nameHeight/2)
-                 } else {
-                    return height/1.5
-                 }
-            },
-            duration: 3,
+        titleLetterRefs.forEach(letter => {
             
-        });
+        gsap.set(letter.current, {x: x, y: y});
+        
+
+        const floatingFunc: any = () =>  {
+            floating = tl.to(letter.current, {
+            onComplete: floatingFunc,
+            ease: "none",
+            duration: "random(5, 20)",
+            x: x,
+            y: y,
+            repeat: -1,
+            yoyo: true,
+        }, 0);
+        }
+        return floatingFunc();          
+    });
+
+    let tl2 = gsap.timeline();
+        let scrollAnim =  tl2.to(q(".letter"), {
+                x: (): number => {
+                    if (nameRef.current){
+                        let nameWidth = nameRef.current.clientWidth;
+                        return (width/2) - (nameWidth/2);
+                    } else {
+                        return width/1.5
+                    } 
+                },
+                y: (): number  => {
+                    if (nameRef.current){
+                        let nameHeight = nameRef.current.clientHeight;
+                        return (height/2) - (nameHeight/2)
+                    } else {
+                        return height/1.5
+                    }
+                },
+                duration: 3,
+            });
+            tl2.to(q(".title"), {
+                x: (): number => {
+                    if (titleRef.current){
+                        let titleWidth = titleRef.current.clientWidth;
+                        return (width/4) - (titleWidth/4);
+                    } else {
+                        return width/1.5
+                    } 
+                },
+                y: (): number  => {
+                    if (titleRef.current){
+                        let titleHeight = titleRef.current.clientHeight;
+                        return (height/4) - (titleHeight/4)
+                    } else {
+                        return height/1.5
+                    }
+                },
+                duration: 3,
+                scale: ".5",
+            }, 0);
+    
         
         
         ScrollTrigger.create ({
             animation: scrollAnim,
-            onEnter: () => {tl.pause()},
+            onEnter: () => {
+                setScrollState(true);
+                tl.pause();
+            },
             trigger: ".container",
             start: "+=1",
             end: "+=400",
@@ -87,17 +137,44 @@ const Header = () => {
             // pinSpacing: false,
         });
     });
+        
+    
 
         return () => ctx.revert(); 
     }, []);
-     
+    
+    
+    const handleMouseMove = (e: any) => {
+        let ctx = gsap.context(() => {
+            if (!scrollState){
+                let xTo = gsap.quickTo(q(".letter"), "x", {duration: 1, ease: "easeOut"}),
+                    yTo = gsap.quickTo(q(".letter"), "y", {duration: 1, ease: "easeOut"});
+
+                xTo(e.pageX);
+                yTo(e.pageY);
+                yTo.tween.pause(0.8);
+            }
+        });
+        return () => ctx.revert();
+    }
     return(
-        <div className="container" ref={container}>
-            <div className="name" ref ={nameRef}>
+        <div className="container" ref={container} onMouseMove={(e) => {handleMouseMove(e)}}>
+            <div className="name" ref={nameRef}>
             {
             nameArr.map((value, index) => {
                 const newLetterRef = createRef<HTMLDivElement>();
-                letterRefs[index] = newLetterRef;
+                nameLetterRefs[index] = newLetterRef;
+                return <div key={index} className="letter" ref={newLetterRef}>
+                    <div>{value}</div>
+                </div>
+                })
+            }
+            </div>
+            <div className="title" ref={titleRef}>
+            {
+            titleArr.map((value, index) => {
+                const newLetterRef = createRef<HTMLDivElement>();
+                titleLetterRefs[index] = newLetterRef;
                 return <div key={index} className="letter" ref={newLetterRef}>
                     <div>{value}</div>
                 </div>
