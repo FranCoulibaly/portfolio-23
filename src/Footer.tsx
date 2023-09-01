d54426a0b2a512f6312ded28a538b45d8565b705
Franc <fran.coulibaly@gmail.com>
2023-09-01 14:12:09 +0200

    Update Footer.tsx

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -39,8 +39,8 @@ const Footer = () => {
             
                 { showWork ? (
                 <div className="workItems">
-                    {workItems.map((item, index) => {
-                        <a href={item.url} target="_blank" className={`workItem ${item.classNum}`}>0{index}</a>
+                    { workItems.map((item, index) => {
+                        <a href={item.url} target="_blank" className={`workItem ${item.classNum}`}>0{index}</a>                                     
                     })}
 {/*                     <a href="https://apollinaris.co.nz/" target="_blank" className="workItem one">01</a>
                     <a href="https://francesyarns.co.nz/" target="_blank" className="workItem two">02</a>
